e4cdb53022b544010216eee718ac4f9a31a04652
emilio <emiliocortina@gmail.com>
2019-05-15 12:36:58 +0200

    Changes in work page

--- js/work.js
+++ js/work.js
@@ -13,3 +13,15 @@ $('#phoenixGallery').click(function(){
         items: ['/resources/projects/phoenix/phoenix1.png', '/resources/projects/phoenix/phoenix2.png']
     });
 })
+
+$('#wpGallery').click(function(){
+    SimpleLightbox.open({
+        items: ['/resources/projects/myWallapop1/myWallapop1_login.png']
+    });
+})
+
+$('#wp2Gallery').click(function(){
+    SimpleLightbox.open({
+        items: ['/resources/projects/myWallapop2/myWallapop2_login.png']
+    });
+})
