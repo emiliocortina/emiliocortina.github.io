13dbade12a15c3a5c160ad357664a10ac24808ca
Emilio <emiliocortina@gmail.com>
2019-09-10 19:40:22 +0200

    Updating Phoenix card inside work section

--- js/work.js
+++ js/work.js
@@ -2,25 +2,25 @@ $('.projectDescription').each(function(index, element) {
     $clamp(element, { clamp: 5, useNativeClamp: true });
 });
 
-$('#soleGallery').click(function(){
+$('#soleGallery').click(function() {
     SimpleLightbox.open({
         items: ['/resources/projects/sole/sole1.png', '/resources/projects/sole/sole2.png', '/resources/projects/sole/sole3.png', '/resources/projects/sole/sole4.png']
     });
 })
 
-$('#phoenixGallery').click(function(){
+$('#phoenixGallery').click(function() {
     SimpleLightbox.open({
-        items: ['/resources/projects/phoenix/phoenix1.png', '/resources/projects/phoenix/phoenix2.png']
+        items: ['/resources/projects/phoenix/phoenix4.jpg', '/resources/projects/phoenix/phoenix6.jpg', '/resources/projects/phoenix/phoenix5.jpg', '/resources/projects/phoenix/phoenix1.jpg', '/resources/projects/phoenix/phoenix2.jpg', '/resources/projects/phoenix/phoenix3.jpg']
     });
 })
 
-$('#wpGallery').click(function(){
+$('#wpGallery').click(function() {
     SimpleLightbox.open({
         items: ['/resources/projects/myWallapop1/myWallapop1_login.png']
     });
 })
 
-$('#wp2Gallery').click(function(){
+$('#wp2Gallery').click(function() {
     SimpleLightbox.open({
         items: ['/resources/projects/myWallapop2/myWallapop2_login.png']
     });
